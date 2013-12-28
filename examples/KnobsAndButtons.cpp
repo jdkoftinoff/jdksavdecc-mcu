11462581c292df7d214856fdda09c657a48dcca5
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2013-12-28 11:46:09 -0800

    KnobsAndButtons example updated

---
+++ examples/KnobsAndButtons.cpp
@@ -0,0 +1,217 @@
+#include "JDKSAvdeccWorld.h"
+#include "Ethernet.h"
+#include "SPI.h"
+#include "JDKSAvdeccWizNetIO.h"
+#include "JDKSAvdeccFrame.h"
+#include "JDKSAvdeccADPManager.h"
+#include "JDKSAvdeccControlSender.h"
+
+using namespace JDKSAvdecc;
+
+/// This MAC address is within the IEEE Std 1722-2011 assigned range, OUI 90-E0-F0 and is only for example usages.
+jdksavdecc_eui48 my_mac = { { 0x90, 0xe0, 0xf0, 0x00, 0x00, 0x01 } };
+
+/// This AVDECC Entity is based on the example MAC address, convered to an EUI-64 by inserting 0xff, 0xfe in the middle
+jdksavdecc_eui64 my_entity_id = { {0x90, 0xe0, 0xf0, 0xff, 0xfe, 0x00, 0x01 } };
+
+/// This AVDECC Entity Model ID is for example usages
+jdksavdecc_eui64 my_entity_model_id = { {0x90, 0xe0, 0xf0, 0xff, 0xfe, 0x00, 0x02 } };
+
+/// the W5100 chip Raw Ethernet handler object
+WizNetIO rawnet(my_mac);
+
+/// The ADP manager is told about the entity id, model_id, entity capabilities, controller capabilities, and valid time in seconds
+ADPManager adp_manager(
+                       rawnet,
+                       my_entity_id,
+                       my_entity_model_id,
+                       JDKSAVDECC_ADP_ENTITY_CAPABILITY_AEM_SUPPORTED,
+                       JDKSAVDECC_ADP_CONTROLLER_CAPABILITY_IMPLEMENTED,
+                       20
+                       );
+
+/// All of the controls are targetting an example AVDECC Entity ID: 90-e0-f0-ff-fe-00-00-02
+jdksavdecc_eui64 target_entity_id = { { 0x90, 0xe0, 0xf0, 0xff, 0xfe, 0x00, 0x00, 0x02 } };
+
+/// All of the controls are targetting an example MAC address: 90-e0-f0-00-00-02
+jdksavdecc_eui48 target_mac_address = { { 0x90, 0xe0, 0xf0, 0x00, 0x00, 0x02 } };
+
+/// The shared sequence ID for transmitted messages
+uint16_t sequence_id = 0;
+
+/// The mapping of Knob 1 to control descriptor 0x0000. 2 byte payload, refresh time of 1000 ms
+ControlSender knob1(
+                    rawnet,
+                    my_entity_id,
+                    target_entity_id,
+                    target_mac_address,
+                    sequence_id,
+                    0x0000,
+                    2,
+                    1000
+                    );
+
+/// The mapping of Knob 2 to control descriptor 0x0001. 2 byte payload, refresh time of 1000 ms
+ControlSender knob2(
+                    rawnet,
+                    my_entity_id,
+                    target_entity_id,
+                    target_mac_address,
+                    sequence_id,
+                    0x0001,
+                    2,
+                    1000
+                    );
+
+/// The mapping of Knob 2 to control descriptor 0x0002. 2 byte payload, refresh time of 1000 ms
+ControlSender knob3(
+                    rawnet,
+                    my_entity_id,
+                    target_entity_id,
+                    target_mac_address,
+                    sequence_id,
+                    0x0002,
+                    2,
+                    1000
+                    );
+
+/// The mapping of Button 1 to control descriptor 0x0003. 2 byte payload, refresh time of 1000 ms
+ControlSender button1(
+                      rawnet,
+                      my_entity_id,
+                      target_entity_id,
+                      target_mac_address,
+                      sequence_id,
+                      0x0003,
+                      1,
+                      1000
+                      );
+
+/// The mapping of Button 2 to control descriptor 0x0004. 2 byte payload, refresh time of 1000 ms
+ControlSender button2(
+                      rawnet,
+                      my_entity_id,
+                      target_entity_id,
+                      target_mac_address,
+                      sequence_id,
+                      0x0004,
+                      1,
+                      1000
+                      );
+
+/// The mapping of Button 3 to control descriptor 0x0005. 2 byte payload, refresh time of 1000 ms
+ControlSender button3(
+                      rawnet,
+                      my_entity_id,
+                      target_entity_id,
+                      target_mac_address,
+                      sequence_id,
+                      0x0005,
+                      1,
+                      1000
+                      );
+
+/// The mapping of Button 3 to control descriptor 0x0006. 2 byte payload, refresh time of 1000 ms
+ControlSender button4(
+                      rawnet,
+                      my_entity_id,
+                      target_entity_id,
+                      target_mac_address,
+                      sequence_id,
+                      0x0006,
+                      1,
+                      1000
+                      );
+
+/// The mapping of Button 3 to control descriptor 0x0007. 2 byte payload, refresh time of 1000 ms
+ControlSender button5(
+                      rawnet,
+                      my_entity_id,
+                      target_entity_id,
+                      target_mac_address,
+                      sequence_id,
+                      0x0007,
+                      1,
+                      1000
+                      );
+
+
+/// The ValueMapper scans the A/D's and Digital input pins at the specified rate
+/// and applies the new values to the appropriate knob or button
+class ValueMapper : public Handler {
+private:
+    /// The update rate in milliseconds
+    uint32_t m_update_rate_in_millis;
+
+    /// The time that the last update happened
+    uint32_t m_last_update_time;
+public:
+
+    ValueMapper( uint32_t update_rate_in_millis )
+    : m_update_rate_in_millis(update_rate_in_millis)
+    , m_last_update_time(0) {
+    }
+
+    virtual void Tick( uint32_t time_in_millis ) {
+        if( time_in_millis > m_last_update_time + m_update_rate_in_millis ) {
+            m_last_update_time = time_in_millis;
+
+            knob1.SetValueDoublet( analogRead(0) << 6 ); // Scale 10 bit analog reading into 0-0xffc0
+            knob2.SetValueDoublet( analogRead(1) << 6 );
+            knob3.SetValueDoublet( analogRead(2) << 6 );
+            
+            button1.SetValueOctet( digitalRead(2) ? 0x00 : 0xff ); // DLI reads true when not pressed, reverse logic
+            button2.SetValueOctet( digitalRead(3) ? 0x00 : 0xff );
+            button3.SetValueOctet( digitalRead(4) ? 0x00 : 0xff );
+            button4.SetValueOctet( digitalRead(5) ? 0x00 : 0xff );  
+            button5.SetValueOctet( digitalRead(6) ? 0x00 : 0xff );  
+            
+            digitalWrite( A5, !digitalRead(2) );
+            digitalWrite( A4, !digitalRead(3) );      
+        }
+    }
+};
+
+/// Create the mapper which polls at 50 ms
+ValueMapper value_mapper(50);
+
+/// Create a HandlerGroup which can manage up to 16 handlers
+HandlerGroup<16> all_handlers(rawnet);
+
+void setup() {
+    // Set up the I/O pins for 3 knobs, 5 buttons, and 2 LED's
+    pinMode(9,OUTPUT);
+    pinMode(2,INPUT_PULLUP);
+    pinMode(3,INPUT_PULLUP);
+    pinMode(4,INPUT_PULLUP);
+    pinMode(5,INPUT_PULLUP);
+    pinMode(6,INPUT_PULLUP);
+    pinMode(A4,OUTPUT);
+    pinMode(A5,OUTPUT);
+    
+    // Initialize the W5100 chip 
+    rawnet.Initialize();
+    
+    // Put all the handlers into the HandlerGroup
+    
+    all_handlers.Add( &value_mapper );
+    all_handlers.Add( &adp_manager );
+    all_handlers.Add( &knob1 );
+    all_handlers.Add( &knob2 );
+    all_handlers.Add( &knob3 );
+    all_handlers.Add( &button1 );
+    all_handlers.Add( &button2 );
+    all_handlers.Add( &button3 );  
+    all_handlers.Add( &button4 );
+    all_handlers.Add( &button5 );
+}
+
+
+void loop() {
+    // Get the current time in milliseconds
+    uint32_t cur_time = millis();
+    
+    // Tell all the handlers to do their periodic jobs
+    all_handlers.Tick(cur_time);
+}
+
--- examples/KnobsAndButtons/KnobsAndButtons.ino
+++ examples/KnobsAndButtons/KnobsAndButtons.ino
@@ -4,16 +4,23 @@
 #include "JDKSAvdeccWizNetIO.h"
 #include "JDKSAvdeccFrame.h"
 #include "JDKSAvdeccADPManager.h"
-#include "JDKSAvdeccSetControlSender.h"
+#include "JDKSAvdeccControlSender.h"
 
 using namespace JDKSAvdecc;
 
+/// This MAC address is within the IEEE Std 1722-2011 assigned range, OUI 90-E0-F0 and is only for example usages.
 jdksavdecc_eui48 my_mac = { { 0x90, 0xe0, 0xf0, 0x00, 0x00, 0x01 } };
+
+/// This AVDECC Entity is based on the example MAC address, convered to an EUI-64 by inserting 0xff, 0xfe in the middle
 jdksavdecc_eui64 my_entity_id = { {0x90, 0xe0, 0xf0, 0xff, 0xfe, 0x00, 0x01 } };
+
+/// This AVDECC Entity Model ID is for example usages
 jdksavdecc_eui64 my_entity_model_id = { {0x90, 0xe0, 0xf0, 0xff, 0xfe, 0x00, 0x02 } };
 
+/// the W5100 chip Raw Ethernet handler object
 WizNetIO rawnet(my_mac);
 
+/// The ADP manager is told about the entity id, model_id, entity capabilities, controller capabilities, and valid time in seconds
 ADPManager adp_manager( 
   rawnet, 
   my_entity_id, 
@@ -23,11 +30,17 @@ ADPManager adp_manager(
   20
   );
 
+/// All of the controls are targetting an example AVDECC Entity ID: 90-e0-f0-ff-fe-00-00-02
 jdksavdecc_eui64 target_entity_id = { { 0x90, 0xe0, 0xf0, 0xff, 0xfe, 0x00, 0x00, 0x02 } };
+
+/// All of the controls are targetting an example MAC address: 90-e0-f0-00-00-02
 jdksavdecc_eui48 target_mac_address = { { 0x90, 0xe0, 0xf0, 0x00, 0x00, 0x02 } };
+
+/// The shared sequence ID for transmitted messages
 uint16_t sequence_id = 0;
 
-SetControlSender knob1(
+/// The mapping of Knob 1 to control descriptor 0x0000. 2 byte payload, refresh time of 1000 ms
+ControlSender knob1(
   rawnet,
   my_entity_id,
   target_entity_id,
@@ -38,7 +51,8 @@ SetControlSender knob1(
   1000
   );
 
-SetControlSender knob2(
+/// The mapping of Knob 2 to control descriptor 0x0001. 2 byte payload, refresh time of 1000 ms
+ControlSender knob2(
   rawnet,
   my_entity_id,
   target_entity_id,
@@ -49,7 +63,8 @@ SetControlSender knob2(
   1000
   );
 
-SetControlSender knob3(
+/// The mapping of Knob 2 to control descriptor 0x0002. 2 byte payload, refresh time of 1000 ms
+ControlSender knob3(
   rawnet,
   my_entity_id,
   target_entity_id,
@@ -60,7 +75,8 @@ SetControlSender knob3(
   1000
   );
 
-SetControlSender button1(
+/// The mapping of Button 1 to control descriptor 0x0003. 2 byte payload, refresh time of 1000 ms
+ControlSender button1(
   rawnet,
   my_entity_id,
   target_entity_id,
@@ -71,7 +87,8 @@ SetControlSender button1(
   1000
   );
 
-SetControlSender button2(
+/// The mapping of Button 2 to control descriptor 0x0004. 2 byte payload, refresh time of 1000 ms
+ControlSender button2(
   rawnet,
   my_entity_id,
   target_entity_id,
@@ -82,7 +99,8 @@ SetControlSender button2(
   1000
   );
 
-SetControlSender button3(
+/// The mapping of Button 3 to control descriptor 0x0005. 2 byte payload, refresh time of 1000 ms
+ControlSender button3(
   rawnet,
   my_entity_id,
   target_entity_id,
@@ -93,7 +111,8 @@ SetControlSender button3(
   1000
   );
 
-SetControlSender button4(
+/// The mapping of Button 3 to control descriptor 0x0006. 2 byte payload, refresh time of 1000 ms
+ControlSender button4(
   rawnet,
   my_entity_id,
   target_entity_id,
@@ -104,7 +123,8 @@ SetControlSender button4(
   1000
   );
 
-SetControlSender button5(
+/// The mapping of Button 3 to control descriptor 0x0007. 2 byte payload, refresh time of 1000 ms
+ControlSender button5(
   rawnet,
   my_entity_id,
   target_entity_id,
@@ -115,7 +135,51 @@ SetControlSender button5(
   1000
   );
 
+
+/// The ValueMapper scans the A/D's and Digital input pins at the specified rate
+/// and applies the new values to the appropriate knob or button
+class ValueMapper : public Handler {
+private:
+  /// The update rate in milliseconds
+  uint32_t m_update_rate_in_millis;
+  
+  /// The time that the last update happened
+  uint32_t m_last_update_time;
+public:  
+
+  ValueMapper( uint32_t update_rate_in_millis ) 
+  : m_update_rate_in_millis(update_rate_in_millis) 
+  , m_last_update_time(0) {
+  }
+  
+  virtual void Tick( uint32_t time_in_millis ) {
+    if( time_in_millis > m_last_update_time + m_update_rate_in_millis ) {
+      m_last_update_time = time_in_millis;
+      
+      knob1.SetValueDoublet( analogRead(0) << 6 ); // Scale 10 bit analog reading into 0-0xffc0
+      knob2.SetValueDoublet( analogRead(1) << 6 );
+      knob3.SetValueDoublet( analogRead(2) << 6 );
+      
+      button1.SetValueOctet( digitalRead(2) ? 0x00 : 0xff ); // DLI reads true when not pressed, reverse logic
+      button2.SetValueOctet( digitalRead(3) ? 0x00 : 0xff );
+      button3.SetValueOctet( digitalRead(4) ? 0x00 : 0xff );
+      button4.SetValueOctet( digitalRead(5) ? 0x00 : 0xff );  
+      button5.SetValueOctet( digitalRead(6) ? 0x00 : 0xff );  
+  
+      digitalWrite( A5, !digitalRead(2) );
+      digitalWrite( A4, !digitalRead(3) );      
+    }
+  }
+};
+
+/// Create the mapper which polls at 50 ms
+ValueMapper value_mapper(50);
+
+/// Create a HandlerGroup which can manage up to 16 handlers
+HandlerGroup<16> all_handlers(rawnet);
+
 void setup() {
+  // Set up the I/O pins for 3 knobs, 5 buttons, and 2 LED's
   pinMode(9,OUTPUT);
   pinMode(2,INPUT_PULLUP);
   pinMode(3,INPUT_PULLUP);
@@ -124,71 +188,31 @@ void setup() {
   pinMode(6,INPUT_PULLUP);
   pinMode(A4,OUTPUT);
   pinMode(A5,OUTPUT);
+  
+  // Initialize the W5100 chip 
   rawnet.Initialize();
+  
+  // Put all the handlers into the HandlerGroup
+  
+  all_handlers.Add( &value_mapper );
+  all_handlers.Add( &adp_manager );
+  all_handlers.Add( &knob1 );
+  all_handlers.Add( &knob2 );
+  all_handlers.Add( &knob3 );
+  all_handlers.Add( &button1 );
+  all_handlers.Add( &button2 );
+  all_handlers.Add( &button3 );  
+  all_handlers.Add( &button4 );
+  all_handlers.Add( &button5 );
 }
 
 
-
 void loop() {
+  // Get the current time in milliseconds
   uint32_t cur_time = millis();
-  static uint32_t last_poll_switch_time = 0;
   
-  // Is it time to poll the switches and knobs?
-  if( cur_time-last_poll_switch_time > 50 ) {  
-    last_poll_switch_time=cur_time;
-    
-    // Yes, read the knobs and swithes and tell the SetControlSender objects about the new values
-    knob1.SetValueDoublet( analogRead(0) );
-    knob2.SetValueDoublet( analogRead(1) );
-    knob3.SetValueDoublet( analogRead(2) );
-    button1.SetValueOctet( digitalRead(2) ? 0x00 : 0xff );
-    button2.SetValueOctet( digitalRead(3) ? 0x00 : 0xff );
-    button3.SetValueOctet( digitalRead(4) ? 0x00 : 0xff );
-    button4.SetValueOctet( digitalRead(5) ? 0x00 : 0xff );  
-    button5.SetValueOctet( digitalRead(6) ? 0x00 : 0xff );  
-  }
-  
-  // Send any ADP messages  
-  adp_manager.Tick( cur_time );
-  
-  // Send any SET_CONTROL messages
-  knob1.Tick( cur_time );
-  knob2.Tick( cur_time );
-  knob3.Tick( cur_time );
-  button1.Tick( cur_time );
-  button2.Tick( cur_time );
-  button3.Tick( cur_time );
-  button4.Tick( cur_time );
-  button5.Tick( cur_time );
-  
-  // Receive any ethernet frames and handle them  
-  {
-    // up to 640 bytes long
-    uint8_t buf[640];
-    uint16_t len;
-    len = rawnet.ReceiveRawNet(buf,sizeof(buf));
-    if( len>0 ) {
-      
-      bool handled=false;
-      // Is it an AVTP/AVDECC message?
-      if( (buf[12] == ((JDKSAVDECC_AVTP_ETHERTYPE>>8)&0xff))
-        && (buf[13] == ((JDKSAVDECC_AVTP_ETHERTYPE>>0)&0xff))) 
-        {          
-          // Yes, dispatch ADP to the adp manager
-          if( buf[14] == (0x80 + JDKSAVDECC_SUBTYPE_ADP) ) {
-              handled = adp_manager.HandleIncomingADP( buf, len );
-          }
-          // dispatch AECP to the entity
-          if( buf[14] == (0x80 + JDKSAVDECC_SUBTYPE_AECP) ) {
-              // AECP command
-          }          
-          // And if we wanted to deal with ACMP, we would do that here.
-          if( buf[14] == (0x80 + JDKSAVDECC_SUBTYPE_ACMP) ) {
-              // ACMP message  
-          }          
-      }
-    }      
-  } 
+  // Tell all the handlers to do their periodic jobs
+  all_handlers.Tick(cur_time);
 }
 
 
