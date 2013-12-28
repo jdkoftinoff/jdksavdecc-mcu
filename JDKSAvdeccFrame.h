0a1a7d8176fad8d22017f17c4e54e73e31ae5939
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2013-12-27 20:19:52 -0800

    Frame generator helper class added

---
+++ JDKSAvdeccFrame.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#include "JDKSAvdeccWorld.h"
+
+namespace JDKSAvdecc {
+
+template <size_t MaxLen>
+class Frame {
+private:
+    uint8_t m_buf[MaxLen];
+    uint16_t m_pos;
+public:
+    Frame(
+          jdksavdecc_eui48 const &dest_mac,
+          jdksavdecc_eui48 const &src_mac,
+          uint16_t ethertype ) : m_pos(0) {
+        PutEUI48( dest_mac );
+        PutEUI48( src_mac );
+        PutDoublet( ethertype );
+    }
+
+    void PutEUI48( jdksavdecc_eui48 const &val ) {
+        for( uint16_t i=0; i<6; ++i ) {
+            m_buf[ m_pos++ ] = val.value[i];
+        }
+    }
+
+    void PutEUI64( jdksavdecc_eui64 const &val ) {
+        for( uint16_t i=0; i<8; ++i ) {
+            m_buf[ m_pos++ ] = val.value[i];
+        }
+    }
+
+    void PutOctet( uint8_t val ) {
+        m_buf[ m_pos++ ] = val;
+    }
+
+    void PutDoublet( uint16_t val ) {
+        m_buf[ m_pos++ ] = (val>>8)&0xff;
+        m_buf[ m_pos++ ] = (val>>0)&0xff;
+    }
+
+    void PutQuadlet( uint32_t val ) {
+        m_buf[ m_pos++ ] = (val>>24)&0xff;
+        m_buf[ m_pos++ ] = (val>>16)&0xff;
+        m_buf[ m_pos++ ] = (val>>8)&0xff;
+        m_buf[ m_pos++ ] = (val>>0)&0xff;
+    }
+
+    void PutOctlet( uint64_t val ) {
+        m_buf[ m_pos++ ] = (val>>56)&0xff;
+        m_buf[ m_pos++ ] = (val>>48)&0xff;
+        m_buf[ m_pos++ ] = (val>>40)&0xff;
+        m_buf[ m_pos++ ] = (val>>32)&0xff;
+        m_buf[ m_pos++ ] = (val>>24)&0xff;
+        m_buf[ m_pos++ ] = (val>>16)&0xff;
+        m_buf[ m_pos++ ] = (val>>8)&0xff;
+        m_buf[ m_pos++ ] = (val>>0)&0xff;
+    }
+
+    void PutZeros( uint16_t count ) {
+        for( uint16_t i=0; i<count; ++i ) {
+            m_buf[ m_pos++ ] = 0;
+        }
+    }
+
+    void PutBuf( uint8_t *buf, uint16_t len ) {
+        for( uint16_t i=0; i<len; ++i ) {
+            m_buf[ m_pos++ ] = buf[i];
+        }
+    }
+
+    uint8_t const *GetBuf() const { return m_buf; }
+    uint16_t GetLength() const { return m_pos; }
+};
+
+}
