a7fda03154ad90643fd610a769f627a1e2e0e7c2
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2013-12-28 19:58:14 -0800

    RawNet Reception fixed

--- JDKSAvdeccWizNetIO.cpp
+++ JDKSAvdeccWizNetIO.cpp
@@ -3,6 +3,10 @@
 
 #include "JDKSAvdeccWizNetIO.h"
 
+extern "C" {
+void avr_debug_log(const char *str, uint16_t v );
+}
+
 #ifdef __AVR__
 #include "utility/w5100.h"
 #include "utility/socket.h"
@@ -23,7 +27,7 @@ void WizNetIO::Initialize() {
 
 
 uint16_t WizNetIO::ReceiveRawNet(uint8_t *data,
-                                   uint16_t max_len ) {
+                                 uint16_t max_len ) {
     uint8_t head[2];
     uint16_t data_len=0;
     uint16_t ptr=0;
@@ -36,13 +40,49 @@ uint16_t WizNetIO::ReceiveRawNet(uint8_t *data,
         data_len = (data_len<<8) + head[1] - 2;
         if( data_len<=max_len ) {
             W5100.read_data(0,(uint8_t*) ptr,data,data_len);
+        } else {
+            avr_debug_log("pkt len", data_len );
         }
         ptr += data_len;
         W5100.writeSnRX_RD(0, ptr);
+        W5100.writeSnIR(0, SnIR::RECV);
         W5100.execCmdSn(0, Sock_RECV);
     }
     return data_len <= max_len ? data_len : 0;
 }
+
+#if 0
+uint16_t WizNetIO::ReceiveRawNet(uint8_t *data,
+                                   uint16_t max_len ) {
+    uint8_t head[2];
+    uint16_t data_len=0;
+    uint16_t ptr=0;
+    uint16_t r=0;
+
+    if( W5100.getRXReceivedSize(0)>2 ) {
+        int e=0;
+        e=recv(0,head,2);
+        if( e==2 ) {
+            data_len = head[0];
+            data_len = (data_len<<8) + head[1] - 2;
+
+            if( max_len<data_len ) {
+                e=recv(0,data,data_len);
+                if( e>0 ) {
+                    r=(uint16_t)e;
+                } else {
+                    e=0;
+                }
+            } else {
+                while(recv(0,head,1)==1 ) {
+                    ; // consume extra bytes
+                }
+            }
+        }
+    }
+    return data_len;
+}
+#endif
 
 bool WizNetIO::SendRawNet(uint8_t const *data,
                           uint16_t len ) {
