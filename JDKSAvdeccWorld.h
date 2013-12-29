6844b5cd973d27311ebe9fcfb0b22a7ffc4522ad
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2013-12-28 19:59:32 -0800

    JDKSAvdeccWorld.h updates

--- JDKSAvdeccWorld.h
+++ JDKSAvdeccWorld.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "jdksavdecc-c/include/jdksavdecc.h"
+#include "jdksavdecc-c/include/jdksavdecc_print.h"
+#include "jdksavdecc-c/include/jdksavdecc_pdu_print.h"
 
 #ifdef __ARDUINO__
 
