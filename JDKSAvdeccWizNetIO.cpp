95118d57265ad299080af6fe1bcae91a660eb07b
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2013-12-31 10:36:21 -0800

    Filter incoming ethernet messages before trying to receive the entire PDU from WizNet5100

--- JDKSAvdeccWizNetIO.cpp
+++ JDKSAvdeccWizNetIO.cpp
@@ -31,6 +31,7 @@ uint16_t WizNetIO::ReceiveRawNet(uint8_t *data,
     uint8_t head[2];
     uint16_t data_len=0;
     uint16_t ptr=0;
+    uint16_t r=0;
 
     if( (W5100.readSnIR(0)&SnIR::RECV) !=0 ) {
         ptr = W5100.readSnRX_RD(0);
@@ -38,17 +39,32 @@ uint16_t WizNetIO::ReceiveRawNet(uint8_t *data,
         ptr+=2;
         data_len = head[0];
         data_len = (data_len<<8) + head[1] - 2;
-        if( data_len<=max_len ) {
-            W5100.read_data(0,(uint8_t*) ptr,data,data_len);
-        } else {
-            avr_debug_log("pkt len", data_len );
+        // read the frame header to make sure we care
+        W5100.read_data(0,(uint8_t*)ptr,data,JDKSAVDECC_FRAME_HEADER_LEN);
+        ptr += JDKSAVDECC_FRAME_HEADER_LEN;
+        // we care if it is ethertype 0x22f0
+        if( (data[JDKSAVDECC_FRAME_HEADER_ETHERTYPE_OFFSET+0]==0x22) &&
+             (data[JDKSAVDECC_FRAME_HEADER_ETHERTYPE_OFFSET+1]==0xf0) ) {
+
+            // we care if it is for me or the ADP/ACMP multicast address
+            if( memcmp(&data[JDKSAVDECC_FRAME_HEADER_DA_OFFSET],m_mac_address.value,6)==0 ||
+                    memcmp(&data[JDKSAVDECC_FRAME_HEADER_DA_OFFSET],jdksavdecc_multicast_adp_acmp.value,6)==0) {
+                // ok, then read the rest of the data
+                if( data_len<=max_len ) {
+                    W5100.read_data(0,(uint8_t*) ptr,data+JDKSAVDECC_FRAME_HEADER_LEN,data_len-JDKSAVDECC_FRAME_HEADER_LEN);
+                    r=data_len;
+                } else {
+                    //            avr_debug_log("pkt len", data_len );
+                }
+            }
         }
-        ptr += data_len;
+        // update the pointers in the chip regardless if we read the packet
+        ptr += data_len-JDKSAVDECC_FRAME_HEADER_LEN;
         W5100.writeSnRX_RD(0, ptr);
         W5100.writeSnIR(0, SnIR::RECV);
         W5100.execCmdSn(0, Sock_RECV);
     }
-    return data_len <= max_len ? data_len : 0;
+    return r;
 }
 
 bool WizNetIO::SendRawNet(uint8_t const *data,
