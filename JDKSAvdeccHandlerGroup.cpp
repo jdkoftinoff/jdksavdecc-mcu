35311db431498b43ced46c1d8969f286e2ea8231
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2013-12-31 13:40:13 -0800

    NetIO handles DA/Ethertype filtering, not needed in the HandlerGroup anymore

--- JDKSAvdeccHandlerGroup.cpp
+++ JDKSAvdeccHandlerGroup.cpp
@@ -20,30 +20,12 @@ bool HandlerGroupBase::PollNet( uint32_t time_in_millis ) {
 
     // Make sure we read DA,SA,Ethertype
     if( len>JDKSAVDECC_FRAME_HEADER_LEN ) {
-        // Make sure it is AVTP/AVDECC ethertype
+        // Ok, this PDU is worth spending time on. Send it on to all known Handlers.
 
-        if( (buf[JDKSAVDECC_FRAME_HEADER_ETHERTYPE_OFFSET+0] == ((JDKSAVDECC_AVTP_ETHERTYPE>>8)&0xff))
-           && (buf[JDKSAVDECC_FRAME_HEADER_ETHERTYPE_OFFSET+1] == ((JDKSAVDECC_AVTP_ETHERTYPE>>0)&0xff))) {
-
-            jdksavdecc_eui48 const &my_mac = net->GetMACAddress();
-
-            // Make sure it is either multicast or is for me
-            if( (buf[JDKSAVDECC_FRAME_HEADER_DA_OFFSET+0]&0x1) ||
-               ( buf[JDKSAVDECC_FRAME_HEADER_DA_OFFSET+0] == my_mac.value[0] &&
-                buf[JDKSAVDECC_FRAME_HEADER_DA_OFFSET+1] == my_mac.value[1] &&
-                buf[JDKSAVDECC_FRAME_HEADER_DA_OFFSET+2] == my_mac.value[2] &&
-                buf[JDKSAVDECC_FRAME_HEADER_DA_OFFSET+3] == my_mac.value[3] &&
-                buf[JDKSAVDECC_FRAME_HEADER_DA_OFFSET+4] == my_mac.value[4] &&
-                buf[JDKSAVDECC_FRAME_HEADER_DA_OFFSET+5] == my_mac.value[5] ) )
-            {
-                // Ok, this PDU is worth spending time on. Send it on to all known Handlers.
-
-                m_rx_count++;
-                r=ReceivedPDU( time_in_millis, buf, len );
-                if( r ) {
-                    m_handled_count++;
-                }
-            }
+        m_rx_count++;
+        r=ReceivedPDU( time_in_millis, buf, len );
+        if( r ) {
+            m_handled_count++;
         }
     }
     return r;
