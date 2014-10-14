fd33e73b4f91beb4d8d652175c26d56a3053d5e0
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2014-10-14 11:59:27 -0700

    update make_arduino_release.sh to copy examples

--- make_arduino_release.sh
+++ make_arduino_release.sh
@@ -2,8 +2,10 @@
 
 rm -r -f jdksavdeccmcu-arduino
 mkdir jdksavdeccmcu-arduino
+mkdir jdksavdeccmcu-arduino/examples
 cp include/*.h* jdksavdeccmcu-arduino/
 cp jdksavdecc-c/include/*.h* jdksavdeccmcu-arduino/
 cp src/*.c* jdksavdeccmcu-arduino/
 cp jdksavdecc-c/src/*.c* jdksavdeccmcu-arduino/
+cp examples/*.cpp jdksavdeccmcu-arduino/examples/
 
