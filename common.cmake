116c22d20bd303a3bc96d9c5e07e4f8c9748a801
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2014-11-21 17:13:19 -0800

    allow lack of c++11 to be not fatal

--- common.cmake
+++ common.cmake
@@ -33,7 +33,7 @@ if (${CMAKE_CXX_COMPILER_ID} MATCHES "GNU")
     execute_process(
 	COMMAND ${CMAKE_CXX_COMPILER} -dumpversion OUTPUT_VARIABLE GCC_VERSION)
     if (NOT (GCC_VERSION VERSION_GREATER 4.7 OR GCC_VERSION VERSION_EQUAL 4.7))
-	message(FATAL_ERROR "${PROJECT_NAME} requires g++ 4.7 or greater.")
+	message("${PROJECT_NAME} requires g++ 4.7 or greater for C++11.")
     endif ()
 elseif (${CMAKE_CXX_COMPILER_ID} MATCHES "Clang")
 
